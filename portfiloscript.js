846f661be53581d9f9b1935a3ce55cc270903ef0
sakshijha007 <sakshijha3110@gmail.com>
2026-01-11 11:31:20 +0530

    Add files via upload

---
+++ portfiloscript.js
@@ -0,0 +1,37 @@
+const text = "B.Tech CSE Student | Frontend Developer | Tech Enthusiast";
+let index = 0;
+
+function typeEffect() {
+    if (index < text.length) {
+        document.getElementById("typing").innerHTML += text.charAt(index);
+        index++;
+        setTimeout(typeEffect, 100);
+    }
+}
+typeEffect();
+
+function toggleTheme() {
+    document.body.classList.toggle("dark");
+}
+let display = document.getElementById("display");
+
+function appendValue(value) {
+    display.value += value;
+}
+
+function clearDisplay() {
+    display.value = "";
+}
+
+function deleteLast() {
+    display.value = display.value.slice(0, -1);
+}
+
+function calculate() {
+    try {
+        display.value = eval(display.value);
+    } catch {
+        display.value = "Error";
+    }
+}
+
